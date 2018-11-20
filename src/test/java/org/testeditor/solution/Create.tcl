1e8b3671f52fb38f68583abe40e9a0643629aea1
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-11-20 13:58:11 +0100

    corrected tcl

--- src/test/java/org/testeditor/solution/Create.tcl
+++ src/test/java/org/testeditor/solution/Create.tcl
@@ -9,8 +9,8 @@ package org.testeditor.heroes
   - Browse "http://sut:4200/heroes"
   Component: Heroes
   - Wait "2" seconds
-  - actualName = Read <LastListItem>
-  - assert actualName = "20 Tornado"
+  - lastHeroName = Read <LastListItem>
+  - assert lastHeroName = "20 Tornado"
 
 * When: I create a hero named "Sancho"
   Component: Heroes
