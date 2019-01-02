66f031e5f48d26621d4e60d23ea3c8243bfdeea2
Gunther Bachmann <gunther.bachmann.si@gmail.com>
2019-01-02 09:10:22 +0000

    update file: Minimal.local_backup.tcl

---
+++ src/test/java/org/testeditor/language_examples/Minimal.local_backup.tcl
@@ -0,0 +1,13 @@
+package org.testeditor.language_examples
+
+import org.testeditor.language_examples.MinimalComponent
+
+# Minimal
+
+
+* minimal test step
+Component: MinimalComponent
+- minimalcallwithoutparameter
+- value = getBoolean
+- assert !value
+// some
