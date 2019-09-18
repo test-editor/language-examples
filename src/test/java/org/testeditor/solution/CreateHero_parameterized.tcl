6154ba941e68232416303c6117e1898945109a65
Jan Jelschen <jan.jelschen@itemis.de>
2019-09-18 15:06:28 +0200

    create parameterized test case examples

---
+++ src/test/java/org/testeditor/solution/CreateHero_parameterized.tcl
@@ -0,0 +1,32 @@
+implements org.testeditor.solution.CreateHero_parameterized
+
+Data:
+  Component: org.testeditor.fixture.commons.io.json.ParameterizedTesting
+  - heroes = load from table "
+    | name     | listEntry   |
+    | Sancho   | 21 Sancho   |
+    | Dulcinea | 21 Dulcinea |
+    | Friston  | 21 Friston  |"
+
+* Given: I am on the heroes page
+  Component: org.testeditor.fixture.web.WebBrowser
+  - Start <Firefox>
+  - Browse "http://sut:4200/heroes"
+
+* When: I create a hero
+  Component: org.testeditor.heroes.Heroes
+  - Wait "2" seconds until <Add> is found
+  - Click <Add>
+  - Enter @heroes.name into <Name>
+  - Wait "2" seconds until <Save> is found
+  - Click <Save>
+
+* Then: That hero should be the last one of the list
+  Component: org.testeditor.heroes.Heroes
+  - Wait "2" seconds
+  - actualName = Read <LastListItem>
+  - assert actualName = heroes.listEntry
+
+  Component: org.testeditor.fixture.web.WebBrowser
+  - Close browser
+  
---
+++ src/test/java/org/testeditor/solution/CreateHero_parameterized_json.tcl
@@ -0,0 +1,28 @@
+implements org.testeditor.solution.CreateHero_parameterized
+
+Data:
+  Component: org.testeditor.fixture.commons.io.json.ParameterizedTesting
+  - heroes =  load from JSON file "org/testeditor/solution/heroes.json"
+
+* Given: I am on the heroes page
+  Component: org.testeditor.fixture.web.WebBrowser
+  - Start <Firefox>
+  - Browse "http://sut:4200/heroes"
+
+* When: I create a hero
+  Component: org.testeditor.heroes.Heroes
+  - Wait "2" seconds until <Add> is found
+  - Click <Add>
+  - Enter @heroes.name into <Name>
+  - Wait "2" seconds until <Save> is found
+  - Click <Save>
+
+* Then: That hero should be the last one of the list
+  Component: org.testeditor.heroes.Heroes
+  - Wait "2" seconds
+  - actualName = Read <LastListItem>
+  - assert actualName = heroes.listEntry
+
+  Component: org.testeditor.fixture.web.WebBrowser
+  - Close browser
+  
